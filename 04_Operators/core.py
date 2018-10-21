2f7365c19089787f7fdc3a15cf8e16d12d288c50
Michael Johann <michaeljohann94@outlook.com>
2018-10-21 03:34:01 +0200

    added Operators

---
+++ 04_Operators/core.py
@@ -0,0 +1,44 @@
+def main():
+    pass
+
+
+def arithmetic():
+    a = 1 + 2           # addion
+    b = 1 - 2           # subtraction
+    c = a * b           # multipication
+    d = c / b           # divison
+
+    mudolus = d % c     # mudolus (divides d by c and reutrns the remainder)
+    exp = **a           # exp(a) multiplys a with itself
+    fldiv = a//2        # divides a by 2 and floors it
+
+
+def comarison():
+    cond = True
+    if cond == True:                                # ==  -> is eaual to
+        print("true is always eaqual to true")
+    if cond != False:                               # !=  -> is not equal to
+        print("cond is not false")
+    if 1 < 2:                                       # <   -> less than
+        print("1 is less than 2")
+    if 2 > 1:                                       # >   -> more than
+        print("2 is more than 1")
+    if 2 <= 3:                                      # <=  -> more or eaqual than/to
+        print("2 is less or eaqual to 3")
+    if 4 >= 3:                                      # >=  -> less or eaqual then/to
+        print("4 is more ore equal to 3")
+
+
+def assignment_operators():
+    a += 2          # a = a + 2
+    b -= 2          # b = b - 2
+    c *= 2          # c = c * 2
+    d /= 2          # d = d / 2
+
+    e %= 2          # e / 2 » e = remainder
+    f **= 2         # f = f²
+    g //= 2         # g = g // 2
+
+
+if __name__ == '__main__':
+    main()
