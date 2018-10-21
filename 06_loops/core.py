f44ad16f1cbf8262a98dfaee105d8aa14e5b5b23
Michael Johann <michaeljohann94@outlook.com>
2018-10-21 04:55:15 +0200

    added for-loop

---
+++ 06_loops/core.py
@@ -0,0 +1,15 @@
+def main():
+    for_loops()
+
+
+def for_loops():
+    tmp = 0
+    tmp_dict = {"cars": ("honda"),
+                "owner": ("Michael")}
+
+    for l in range(0, 2000, 1):
+        tmp += l
+    # TODO: Nested for-Loop
+
+if __name__ == '__main__':
+    main()
