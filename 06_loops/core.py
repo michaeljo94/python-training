134b5f3470f94ef1d1f6b1b291dc2bebf6e1ae77
Michael Johann <michaeljohann94@outlook.com>
2018-10-21 20:09:31 +0200

    added documentation

--- 06_loops/core.py
+++ 06_loops/core.py
@@ -8,10 +8,15 @@ def for_loops():
     tmp_list = ["Hallo", "World"]
 
     for l in range(0, 2000, 1):
-        tmp += l
+        '''
+            it itarates through a list of numers from 0 to 1999 with
+             a step-size of 1
+            l represents the object which is iterated.
+        '''
+        tmp += l                # tmp = tmp + l
 
     # Nested for-loop
-    for i in tmp_list:
+    for i in tmp_list:          
         for j in i:
             print(j)
 
@@ -20,7 +25,7 @@ def while_loops():
     Jesus_is_alive = False
     you_are_beliver = False
 
-    while not Jesus_is_alive:
+    while not Jesus_is_alive:  # it repeats as long Jesus_is_alive is not True
         print("Ahmen")
         if you_are_beliver:
             Jesus_is_alive = True
