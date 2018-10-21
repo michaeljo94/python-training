3e85ef89e273e432f1aee5d3adb8825ff23e743f
Michael Johann <michaeljohann94@outlook.com>
2018-10-21 20:03:32 +0200

    added while_loops() TODO: Documentation

--- 06_loops/core.py
+++ 06_loops/core.py
@@ -1,15 +1,30 @@
 def main():
     for_loops()
+    while_loops()
 
 
 def for_loops():
     tmp = 0
-    tmp_dict = {"cars": ("honda"),
-                "owner": ("Michael")}
+    tmp_list = ["Hallo", "World"]
 
     for l in range(0, 2000, 1):
         tmp += l
-    # TODO: Nested for-Loop
+
+    # Nested for-loop
+    for i in tmp_list:
+        for j in i:
+            print(j)
+
+
+def while_loops():
+    Jesus_is_alive = False
+    you_are_beliver = False
+
+    while not Jesus_is_alive:
+        print("Ahmen")
+        if you_are_beliver:
+            Jesus_is_alive = True
+        you_are_beliver = True
 
 if __name__ == '__main__':
     main()
