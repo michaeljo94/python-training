fb20a6d3c9ff8fe4d50d0e2734b268df678f4255
Michael Johann <michael.j1@schule.bremen.de>
2020-10-30 18:53:50 +0100

    styke(doc): fixed formating

--- 01_Hello_World/core.py
+++ 01_Hello_World/core.py
@@ -6,11 +6,10 @@ def main():
     # Prints a variable to the console
     print("Hello World!")
 
-#
 # if __name__ is "__main__" the content inside is to be run.
 # __name__ is a "magic" variable which contains the name of
-# the current Scope on importlevel
-#
+# the current Scope at importlevel
 if __name__ == '__main__':
+
     # function call invoking main()
     main()
