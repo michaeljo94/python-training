9502db0e207332a00da17fb0e1aa4aee55aa006c
Michael Johann <michael.j1@schule.bremen.de>
2020-10-30 18:56:34 +0100

    feat(doc): added explanation
    added some explamations for every statement necessary

--- 02_getting_input/core.py
+++ 02_getting_input/core.py
@@ -1,7 +1,16 @@
+
+#function main
 def main():
+
+    # input(x: str) grabs a string from std:in
+    # and returns the entered input.
     name = input("Who are you, dude?\n")
+
+    # concats the string literal with the
+    # contents of the name variable above
     print("hi there, " + name)
 
-
+# ifmain entrypoint
 if __name__ == '__main__':
+    # calls function main()
     main()
