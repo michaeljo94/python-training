69c8ebaf769190c14b8253e2a2557ecb85b3741c
Michael Johann <michael.j1@schule.bremen.de>
2020-10-30 18:38:46 +0100

    doc(expl) explanaion added
    Added explaination for each statement

--- 01_Hello_World/core.py
+++ 01_Hello_World/core.py
@@ -1,6 +1,16 @@
+# Module
+
+# Function declaration
 def main():
+
+    # Prints a variable to the console
     print("Hello World!")
 
-
+#
+# if __name__ is "__main__" the content inside is to be run.
+# __name__ is a "magic" variable which contains the name of
+# the current Scope on importlevel
+#
 if __name__ == '__main__':
+    # function call invoking main()
     main()
